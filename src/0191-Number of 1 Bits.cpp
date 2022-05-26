bdebc18e7ab53f15b84a01e3d129213a5fb16baf
irikong <gookirix@gmail.com>
2022-05-26 14:59:52 +0900

    220526

---
+++ src/0191-Number of 1 Bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ret = 0;
+
+        while (n) {
+            ret += n & 1;
+            n >>= 1;
+        }
+
+        return ret;
+    }
+};
