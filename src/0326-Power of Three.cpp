c814c79917bb59328123369b5a24f1709cc55492
irikong <gookirix@gmail.com>
2022-08-24 16:12:11 +0900

    220824

--- src/0326-Power of Three.cpp
+++ src/0326-Power of Three.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        while (n > 1) {
-            if (n % 3) return false;
-            n /= 3;
-        }
+        int maxPower = pow(3, 19);
 
-        return n == 1;
+        return !(maxPower % n);
     }
 };
